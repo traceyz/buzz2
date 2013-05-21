e90b254a008170b515dcab41701c651b4b3d3b71
Tracey Zellmann <tzellmann@rbmtechnologies.rbm>
2013-05-21 11:21:01 -0400

    moved product creation into load_data, and needed to make product name accessible

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
   has_many :product_links
   belongs_to :category
 
-  attr_accessible [ :name ]
+  attr_accessible :name
 
   validates :name,  presence: true, uniqueness: true
 
--- lib/load_data/load_data.rb
+++ lib/load_data/load_data.rb
@@ -1,11 +1,56 @@
 module LoadData
 
   def load_products
-    data = [
-        ["Lifestyle DVD-based Systems",
+    [
+      ["Lifestyle DVD-based Systems",
         ["LS 12", "LS 18", "LS 28", "LS 30", "LS 35", "LS 38", "LS 48"]
+      ],
+      ["Lifestyle Component-based Systems",
+        ["LS 135", "LS 235", "LS T20", "LS V10", "LS V20", "LS V25", "LS V30", "LS V35"]
+      ],
+      ["321 Systems",
+        ["321", "321 GS"]
+      ],
+      ["Home Theater Speakers",
+        ["AM 10", "AM 15", "AM 16", "AM 6", "AM 7", "Acoustimass DirectReflecting Speaker",
+        "CineMate", "Cinemate 1 SR", "Jewel Cube Speakers", "VCS 10"]
+      ],
+      ["TV Speakers",
+        ["Bose Solo"]
+      ],
+      ["Multimedia Speakers",
+        ["Companion 2", "Companion 20", "Companion 3", "Companion 5", "Computer Music Monitor",
+         "MediaMate", "SoundDock", "SoundDock 10", "SoundDock III", "SoundDock Portable",
+         "SoundLink Air", "SoundLink Bluetooth", "Wireless Computer Speaker"]
+      ],
+      ["Stereo Speakers",
+        ["101", "141", "151", "161", "191", "201", "301", "501", "601",
+          "6.2", "791", "802", "901", "AM 3", "AM 5"]
+      ],
+      ["Outdoor Speakers",
+        ["131", "251", "FS 51"]
+      ],
+      ["Wave Products",
+        ["AWMS", "Wave Control Pod", "Wave Radio", "Wave Radio CD", "Wave music system"]
+      ],
+      ["Headphones",
+         ["AE2", "AE2w", "Bluetooth Headset", "Bluetooth Headset Series 2",
+          "Bose Around-ear", "Bose In-ear", "Bose On-ear", "IE2", "MIE2",
+          "Mobile In-Ear Headset", "Mobile On-Ear Headset",
+          "OE2", "QC 15", "QC 2", "QC 3", "SIE2"]
+      ],
+      ["Video Systems",
+         ["VideoWave", "VideoWave II-46", "VideoWave II-55"]
+      ],
+      ["Lifestyle Accessories",
+        ["SL2"]
       ]
-    ]
+    ].each do |c_name, p_names|
+      puts c_name
+      cat = Category.find_by_name(c_name)
+      p_names.each { |name| cat.products.create!(:name => name) }
+    end
+    nil
   end
 
   def load_forum_links

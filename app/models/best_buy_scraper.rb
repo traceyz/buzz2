26fccffa04324fe260a327949b406e972ef87b4e
Tracey Zellmann <tzellmann@rbmtechnologies.rbm>
2013-05-28 17:43:58 -0400

    loop control to get next page of reviews unitl a review is repeated

--- app/models/best_buy_scraper.rb
+++ app/models/best_buy_scraper.rb
@@ -54,22 +54,32 @@ class BestBuyScraper < Scraper
   end
 
   def self.harvest_reviews
+    reviews = Set.new
     root = 'http://bestbuy.ugc.bazaarvoice.com/3545w/'
-    tail = '/reviews.djs?format=embeddedhtml&page=1&scrollToTop=true%20HTTP/1.1'
+    tail = '/reviews.djs?format=embeddedhtml&page='
+    final = '&scrollToTop=true%20HTTP/1.1'
     forum = Forum.find_by_name("BestBuy")
     forum.product_links.each do |product_link|
       product_link.link_urls.each do |link_url|
+        idx = 1
         link_url.link =~ /skuId=(\d+)&id/
         next unless $1
-        url = root + $1 + tail
-        p = open(url)
-        data = p.read
-        data =~ /var materials=({.+}),\s*initializers=/m
-        json = JSON.parse($1)
-        doc = Nokogiri::HTML(json['BVRRSourceID'])
-        doc.css('div.BVRRContentReview').each do |review|
-          puts "REVIEW"
-          puts review.attr("id").split("_")[1]
+        key = $1
+        more = true
+        while more
+          url = root + key + tail + idx.to_s + final
+          puts url
+          p = open(url)
+          p.read =~ /var materials=({.+}),\s*initializers=/m
+          json = JSON.parse($1)
+          doc = Nokogiri::HTML(json['BVRRSourceID'])
+          doc.css('div.BVRRContentReview').each do |review|
+            r_id = review.attr("id").split("_")[1]
+            puts r_id
+            more = reviews.add?(r_id)
+            break unless more
+          end
+          idx += 1
         end
       end
     end

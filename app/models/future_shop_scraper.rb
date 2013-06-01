7e8b130888fb97b5f33b078e30bc51bde9a90af7
Tracey Zellmann <tracey.zellmann@gmail.com>
2013-06-01 12:58:48 -0400

    working future shop scraper

--- app/models/future_shop_scraper.rb
+++ app/models/future_shop_scraper.rb
@@ -1,23 +1,38 @@
 class FutureShopScraper < Scraper
-  def self.harvest_reviews
 
+  def self.forum
+    Forum.find_by_name("FutureShop")
   end
 
   def self.page_reviews(doc)
-
+    doc.css(".customer-review-item")
   end
 
   def self.get_unique_key(review)
-
+    # future shop does not have a unique reviewer identifier on the page
+    build_unique_key(args_from_review(review))
   end
 
   def self.args_from_review(review)
-    args = {}
+    date = review.at_css(".date").text.strip
+    if date =~ /(\d+) days ago/
+      d = Time.now - ($1.to_i * 60 * 60 * 24)
+      review_date = build_date4("#{d.year}-#{d.month}-#{d.day}")
+    else
+      review_date = build_date(date.strip)
+    end
+    {
+      rating: review.at_css(".rating-score").text.to_i,
+      review_date: review_date,
+      headline: review.at_css(".customer-review-title").text.strip,
+      author: review.at_css(".name").text.strip,
+      location: review.at_css(".loc").text.strip.sub(/from /,''),
+      body: review.at_css("p").text.strip
 
-    args
+    }
   end
 
-  def self.next_link(doc)
-    "NOT KNOWN"
+  def self.next_link(doc,link_url,url,klass)
+    nil
   end
 end

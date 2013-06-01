4c3e3bf469ca525fdb152ca543bb77a50eb64732
Tracey Zellmann <tracey.zellmann@gmail.com>
2013-06-01 12:23:34 -0400

    cnet scraper working

--- app/models/cnet_scraper.rb
+++ app/models/cnet_scraper.rb
@@ -1,23 +1,46 @@
 class CnetScraper < Scraper
-  def self.harvest_reviews
 
+  def self.forum
+    Forum.find_by_name("Cnet")
   end
 
-  def self.page_reviews(doc)
 
+  def self.page_reviews(doc)
+    doc.css(".rateSum")
   end
 
   def self.get_unique_key(review)
-
+    review.attr("messageid")
   end
 
   def self.args_from_review(review)
-    args = {}
-
-    args
+    review_string = review.at_css(".userReviewBody").to_s
+    pros = review_string =~ /Pros.?<\/strong>(.+?)<\/p>/sm ? "Pros: #{$1} " : ""
+    cons = review_string =~ /Cons.?<\/strong>(.+?)<\/p>/sm ? "Cons: #{$1} " : ""
+    summary = review_string =~ /Summary.?<\/strong>(.+?)<\/p>/sm ? $1 : ""
+    body = "#{pros}#{cons}#{summary}".gsub(/<[^>]+>/,"")
+    begin
+      author_elt = review.at_css(".author").text
+    rescue
+      author_elt = ""
+    end
+    {
+      rating: review.at_css(".stars").text.sub(/stars/,"").strip.to_i,
+      headline: review.at_css(".userRevTitle").text.gsub(/\"/, "").strip,
+      author: author_elt,
+      location: "NA",
+      review_date: build_date(review.at_css("time").text),
+      body: body
+    }
   end
 
-  def self.next_link(doc)
-    "NOT KNOWN"
+  def self.next_link(doc,link_url,url,klass)
+    begin
+      link = URI::encode("http://reviews.cnet.com#{doc.at_css('li.next a')[:href]}")
+      puts "NEXT LINK #{link}"
+    rescue
+      puts "NO NEXT LINK"
+    end
+    link
   end
 end

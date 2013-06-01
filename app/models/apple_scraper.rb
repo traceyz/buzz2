41bf7e3d843d54cd407cfa5e96f5879c2f3808cd
Tracey Zellmann <tracey.zellmann@gmail.com>
2013-06-01 08:29:33 -0400

    clean up the args_from_review method

--- app/models/apple_scraper.rb
+++ app/models/apple_scraper.rb
@@ -1,7 +1,7 @@
 class AppleScraper < Scraper
 
-  def self.harvest_reviews
-    get_reviews(Forum.find_by_name("Apple"))
+  def self.forum
+    Forum.find_by_name("Apple")
   end
 
   def self.page_reviews(doc)
@@ -13,20 +13,26 @@ class AppleScraper < Scraper
   end
 
   def self.args_from_review(review)
-    args = {}
-    args[:rating] = review.css('span[@itemprop="ratingValue"]').text.to_i
-    args[:headline] = review.css('h2.summary').text.strip
-    args[:author] = review.css('span[@itemprop="author"]').text
-    str = review.css('ul.statistics > li').first.text.strip
-    args[:location] = str =~ /.+from (.+)\Z/ ? $1 : "NA"
+    location_str = review.css('ul.statistics > li').first.text.strip
     date_str = review.css('time').attr('datetime').text
-    args[:review_date] = build_date4(date_str)
-    args[:body] = review.css('p.description').text.strip
-    args
+    {
+      :rating => review.css('span[@itemprop="ratingValue"]').text.to_i,
+      :headline => review.css('h2.summary').text.strip,
+      :author => review.css('span[@itemprop="author"]').text,
+      :location => location_str =~ /.+from (.+)\Z/ ? $1 : "NA",
+      :review_date => build_date4(date_str),
+      :body => review.css('p.description').text.strip
+    }
   end
 
-  def self.next_link(doc)
-    "NOT KNOWN"
+  def self.next_link(doc,link_url,url,klass)
+    link = nil
+    begin
+      link =  "http://store.apple.com#{doc.css('li.next a')[0][:href]}"
+    rescue => e
+      puts "NO NEXT LINK"
+    end
+    link
   end
 
 end

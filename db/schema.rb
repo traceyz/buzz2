5c61e23c0fb92adaf10c193e3af126bc1e5900c0
Tracey Zellmann <tracey.zellmann@gmail.com>
2013-11-24 18:27:58 -0500

    latest migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130609164028) do
+ActiveRecord::Schema.define(:version => 20131124181515) do
 
   create_table "categories", :force => true do |t|
-    t.string   "name",       :null => false
-    t.integer  "position",   :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "name",        :null => false
+    t.integer  "position",    :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.integer  "category_id"
   end
 
   add_index "categories", ["name"], :name => "index_categories_on_name", :unique => true
